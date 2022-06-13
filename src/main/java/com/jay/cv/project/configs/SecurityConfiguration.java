2a3b39d0c61b5032d6f14159d7b48a6886d2785b
prekjgnan <pjayamayooragan2016@gmail.com>
2022-06-13 15:55:55 +0100

    Added security#

---
+++ src/main/java/com/jay/cv/project/configs/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.jay.cv.project.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+}
